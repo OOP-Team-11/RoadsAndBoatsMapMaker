975aa074623d6b0c418e45f4fcb95e41dce9a50e
konradpabjan <konradpabjan@outlook.com>
2017-04-12 00:25:14 -0400

    Setup Render Objects and obserers for views & proper switching

--- src/main/game/view/SaveLoadView.java
+++ src/main/game/view/SaveLoadView.java
@@ -2,18 +2,28 @@ package game.view;
 
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Font;
 
 public class SaveLoadView extends View {
 
     private AnchorPane anchorPane;
 
     public SaveLoadView(AnchorPane anchorPane){
-        this.anchorPane = anchorPane;
+        setAnchorPane(anchorPane);
+        drawHeadingTitle();
+    }
 
+    private void setAnchorPane(AnchorPane anchorPane){
+        this.anchorPane = anchorPane;
+    }
+
+    private void drawHeadingTitle(){
         // for Testing atm, remove later
         Label label = new Label();
-        label.setText("SaveLoadView");
+        label.setText("Save Load Game");
+        label.setFont(new Font(60));
         this.anchorPane.getChildren().add(label);
+        this.anchorPane.setLeftAnchor(label,450.0);
     }
 
     @Override
